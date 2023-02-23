239e6ca50b6fb79ef3bb360ed75d16aa8d7f1a08
Lucas Pickering <lucas@lucaspickering.me>
2023-02-23 18:50:44 -0500

    Add lazily evaluated commands

--- src/shell.rs
+++ src/shell.rs
@@ -1,4 +1,4 @@
-use crate::config::Profile;
+use crate::config::{Profile, Value};
 use anyhow::bail;
 use std::{env, ffi::OsStr, path::PathBuf};
 
@@ -38,14 +38,33 @@ impl Shell {
     /// Get the shell command that will set a variable to a particular value for
     /// this shell type. This command should be piped to the source command, as
     /// defined by [Self::source_command] to apply it in the present shell.
-    pub fn export_variable(&self, variable: &str, value: &str) -> String {
-        // Run a shell command to export the variable
+    pub fn export_variable(&self, variable: &str, value: &Value) -> String {
+        let value = self.value_to_string(value);
+        // Generate a shell command to export the variable
         match self {
-            // Single quotes are needed to prevent injection vulnerabilities
+            // Single quotes are needed to prevent injection vulnerabilities.
+            // Quotes on the value are applied by value_to_string, as necessary
             Self::Bash | Self::Zsh => {
-                format!("export '{}'='{}'", variable, value)
+                format!("export '{variable}'={value}")
             }
-            Self::Fish => format!("set -x '{variable}' '{value}'"),
+            Self::Fish => format!("set -x '{variable}' {value}"),
+        }
+    }
+
+    /// Map a value to a string that can be processed by the shell. Either a
+    /// literal value or a subshell command to get a dynamic value.
+    pub fn value_to_string(&self, value: &Value) -> String {
+        match (self, value) {
+            // Include single quotes to prevent accidental injection
+            (_, Value::Literal(value)) => format!("'{value}'"),
+            // Unfortunately no way around injection on these, since it _is_ an
+            // injection
+            (Self::Bash | Self::Zsh, Value::Command { command }) => {
+                format!("\"$({command})\"")
+            }
+            (Self::Fish, Value::Command { command }) => {
+                format!("({command})")
+            }
         }
     }
 
