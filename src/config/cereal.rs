54b23009abcd99641afea5ddc232adaf870d5a85
Lucas Pickering <lucas@lucaspickering.me>
2023-07-05 18:43:39 -0400

    Support sensitive literal values (refactor a lot of config code)
    This required refactoring config code to customize the deserialization. Making a literal value sensitive doesn't make much sense, but pulling common fields out into a parent will make it easier to support future value source types (e.g. kube pods).

---
+++ src/config/cereal.rs
@@ -0,0 +1,45 @@
+//! Config serialization and deserialization
+
+use crate::config::{ValueSource, ValueSourceInner};
+use serde::{
+    de::{self, value::MapAccessDeserializer, MapAccess, Visitor},
+    Deserialize, Deserializer,
+};
+
+impl<'de> Deserialize<'de> for ValueSource {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ValueSourceWrapperVisitor;
+
+        impl<'de> Visitor<'de> for ValueSourceWrapperVisitor {
+            type Value = ValueSource;
+
+            fn expecting(
+                &self,
+                formatter: &mut std::fmt::Formatter,
+            ) -> std::fmt::Result {
+                formatter.write_str("string or map")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(ValueSource::from_literal(value))
+            }
+
+            fn visit_map<M>(self, map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'de>,
+            {
+                Ok(ValueSource(<ValueSourceInner as Deserialize>::deserialize(
+                    MapAccessDeserializer::new(map),
+                )?))
+            }
+        }
+
+        deserializer.deserialize_any(ValueSourceWrapperVisitor)
+    }
+}
---
+++ src/config/merge.rs
@@ -0,0 +1,50 @@
+use super::{Application, Config, Profile};
+use indexmap::{map::Entry, IndexMap, IndexSet};
+use std::hash::Hash;
+
+/// Indicates that two values of this type can be merged together.
+pub trait Merge {
+    /// Merge another value into this one. The "other" value **will take
+    /// precedence** over this one, meaning conflicting values from the incoming
+    /// will overwrite.
+    fn merge(&mut self, other: Self);
+}
+
+impl Merge for Config {
+    fn merge(&mut self, other: Self) {
+        self.variables.merge(other.variables);
+        self.applications.merge(other.applications);
+    }
+}
+
+impl Merge for Application {
+    fn merge(&mut self, other: Self) {
+        self.profiles.merge(other.profiles)
+    }
+}
+
+impl Merge for Profile {
+    fn merge(&mut self, other: Self) {
+        // Incoming entries take priority over ours
+        self.variables.extend(other.variables.into_iter())
+    }
+}
+
+impl<T: Eq + Hash> Merge for IndexSet<T> {
+    fn merge(&mut self, other: Self) {
+        self.extend(other)
+    }
+}
+
+impl<K: Eq + Hash, V: Merge> Merge for IndexMap<K, V> {
+    fn merge(&mut self, other: Self) {
+        for (k, other_v) in other {
+            match self.entry(k) {
+                Entry::Occupied(mut entry) => entry.get_mut().merge(other_v),
+                Entry::Vacant(entry) => {
+                    entry.insert(other_v);
+                }
+            }
+        }
+    }
+}
