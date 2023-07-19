deacec8d8ee8b9c151c5c14dad4cf5b470336333
Lucas Pickering <lucas@lucaspickering.me>
2023-07-19 06:29:02 -0400

    Prompt for application when not given

--- src/console.rs
+++ src/console.rs
@@ -1,47 +1,55 @@
-use crate::config::{Application, Name, Profile};
-use anyhow::bail;
+use crate::config::{Application, DisplayKeys, Name, Profile};
+use anyhow::{anyhow, bail};
 use atty::Stream;
 use dialoguer::{theme::ColorfulTheme, Select};
+use indexmap::IndexMap;
+use std::fmt::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-/// Show a prompt that allows the user to select a profile for an application,
-/// from a given list.
-pub fn prompt_application(
-    application: &Application,
-) -> anyhow::Result<&Profile> {
-    let theme = ColorfulTheme::default();
-    let profiles: Vec<(&Name, &Profile)> =
-        application.profiles.iter().collect();
+/// Prompt the user to select one option from a list.
+pub fn prompt_options<'a, T: Prompt>(
+    options: &'a IndexMap<Name, T>,
+    default_name: Option<&'a Name>,
+) -> anyhow::Result<&'a T> {
+    match default_name {
+        Some(default_name) => options.get(default_name).ok_or_else(|| {
+            anyhow!(
+                "No {} with the name {}, options are: {}",
+                T::SELF_NAME,
+                default_name,
+                options.display_keys()
+            )
+        }),
 
-    if profiles.is_empty() {
-        bail!("No profiles for this application");
+        // Show a prompt to ask the user which profile to use
+        None => {
+            let theme = ColorfulTheme::default();
+            let options_vec = options.iter().collect::<Vec<_>>();
+
+            if options_vec.is_empty() {
+                bail!("No {}s to choose from", T::SELF_NAME);
+            }
+
+            // Show a prompt to ask the user which value to use
+            let chosen_index = Select::with_theme(&theme)
+                .with_prompt(format!("Select {}", T::SELF_NAME))
+                .items(
+                    options_vec
+                        .iter()
+                        .map(|(name, option)| option.format_option(name))
+                        .collect::<Vec<_>>()
+                        .as_slice(),
+                )
+                .default(0)
+                .interact()?;
+
+            // This index is safe because it came from the value array above
+            Ok(options_vec[chosen_index].1)
+        }
     }
-
-    // Show a prompt to ask the user which value to use
-    let chosen_index = Select::with_theme(&theme)
-        .items(
-            profiles
-                .iter()
-                .map(|(name, profile)| {
-                    // First line of the output will be the profile name, then
-                    // we'll show all the variable mappings
-                    let mut buffer = format!("=== {name} ===\n");
-                    for (variable, value) in &profile.variables {
-                        buffer += &format!("{variable} = {value}\n");
-                    }
-                    buffer
-                })
-                .collect::<Vec<_>>()
-                .as_slice(),
-        )
-        .default(0)
-        .interact()?;
-
-    // This index is safe because it came from the value array above
-    Ok(profiles[chosen_index].1)
 }
 
 /// Print the given message, but only if we're connected to a TTY. Normally we
@@ -66,4 +74,40 @@ pub fn print_installation_hint() -> anyhow::Result<()> {
         "Initialize env-select automatically on shell startup: \
             {REPOSITORY}/tree/v{VERSION}#configure-your-shell",
     ))
+}
+
+pub trait Prompt: Sized {
+    const SELF_NAME: &'static str;
+
+    fn format_option(&self, name: &Name) -> String;
+}
+
+impl Prompt for Application {
+    const SELF_NAME: &'static str = "application";
+
+    fn format_option(&self, name: &Name) -> String {
+        // First line of the output will be the profile name, then
+        // we'll show all the variable mappings
+        let mut buffer = String::new();
+        writeln!(buffer, "=== {name} ===").unwrap();
+        for profile_name in self.profiles.keys() {
+            writeln!(buffer, "{profile_name}").unwrap();
+        }
+        buffer
+    }
+}
+
+impl Prompt for Profile {
+    const SELF_NAME: &'static str = "profile";
+
+    fn format_option(&self, name: &Name) -> String {
+        // First line of the output will be the profile name, then
+        // we'll show all the variable mappings
+        let mut buffer = String::new();
+        writeln!(buffer, "=== {name} ===").unwrap();
+        for (variable, value) in &self.variables {
+            writeln!(buffer, "{variable} = {value}").unwrap();
+        }
+        buffer
+    }
 }
